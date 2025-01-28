9f4eb07d619eeed27a5723a3cd8279e040f66c90
Thomas Lebel <thomas.lebel38@gmail.com>
2025-01-28 23:23:12 +0100

    Test css

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document(ctx) {
-  const { locale } = ctx.__NEXT_DATA__; // Récupère la langue dynamique
-
+export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
-      <body className='font-sans'>
+      <Head></Head>
+      <body className="font-sans">
         <Main />
         <NextScript />
       </body>
